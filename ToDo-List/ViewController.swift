b1413a09ad2ca397ff13135a2590e530e1cb46d5
Ramilyushka <ramiliaamineva@yandex.ru>
2025-11-02 21:45:50 +0500

    Initial Commit

---
+++ ToDo-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo-List
+//
+//  Created by Наиль on 02/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
