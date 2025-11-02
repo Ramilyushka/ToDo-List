b9effa474094784dab4f19fd5e706e9c1bfc4cb3
Ramilyushka <ramiliaamineva@yandex.ru>
2025-11-02 21:56:32 +0500

    first commit

---
+++ ToDo-List/App/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  ToDo-List
+//
+//  Created by Ramilia on 02/11/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        window.rootViewController = MainViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
---
+++ ToDo-List/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ToDo-List
+//
+//  Created by Ramilia on 02/11/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
